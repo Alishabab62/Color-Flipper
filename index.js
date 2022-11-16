3ead1a8a8cbd5fc0d29c4ffe2cbd2af14b635184
AliShabab62 <Alishabab62@gmail.com>
2022-11-16 23:23:27 +0530

    Complete

--- index.js
+++ index.js
@@ -7,8 +7,19 @@ function color_changer(){
     const green=Math.floor(Math.random()*256);
     const blue=Math.floor(Math.random()*256);
     const newrgb=`RGB(${red},${green},${blue})`;
-
-    btn.innerText=newrgb
+    btn.innerText=newrgb;
     body.style.background=newrgb;
-    btn.style.background=newrgb
+    btn.style.background=newrgb;
+
+    const r=Math.floor(Math.random()*256);
+    const g=Math.floor(Math.random()*256);
+    const b=Math.floor(Math.random()*256);
+    const borderrgb=`RGB(${r},${g},${b})`;
+    btn.style.borderColor=borderrgb;
+
+    const rc=Math.floor(Math.random()*256);
+    const gc=Math.floor(Math.random()*256);
+    const bc=Math.floor(Math.random()*256);
+    const colorrgb=`RGB(${rc},${gc},${bc})`;
+    btn.style.color=colorrgb;
 }
